ec36847188ccdff2b896a4d48034aded5fbbe078
Robert Stapleton <robert@stapleton-family.net>
2023-06-21 16:02:59 -0400

    added logical disk info stuff

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -3,3 +3,6 @@ $Name = $env:COMPUTERNAME
 
 # operating system version
 $Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
+
+# logical disk count
+$DiskCount = (Get-CimInstance CIM_LogicalDisk).count
