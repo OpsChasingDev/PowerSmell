b5b27815d6ed492fe9554f5d780eac4f12d2b9a9
Robert Stapleton <robert@stapleton-family.net>
2023-06-28 15:08:50 -0400

    adjusted script to use formatted output

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,8 +1,14 @@
-# retrieve system name
+# retrieve system info
 $Name = $env:COMPUTERNAME
-
-# operating system version
 $Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
-
-# logical disk count
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
+
+# make our own custom object
+$obj = [PSCustomObject]@{
+    ComputerName = $Name
+    OsVersion = $Version
+    DiskCount = $DiskCount
+}
+
+# show output
+Write-Output $obj
