4d62b3027efa3af0d5f23b34b080f005242acb79
Robert Stapleton <robert@stapleton-family.net>
2023-06-28 16:06:50 -0400

    added notes for next class

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,13 +1,22 @@
+# FOR NEXT CLASS 
+<#
+- review people's scripts and see what they've added
+- modify the 'GB Free' property to operate only on the system drive
+- modify the script to reuse the operation for Get-CimInstance CIM_LogicalDisk
+#>
+
 # retrieve system info
 $Name = $env:COMPUTERNAME
 $Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
 $DiskCount = (Get-CimInstance CIM_LogicalDisk).count
+$FreeSpace = (Get-CimInstance CIM_LogicalDisk).FreeSpace
 
 # make our own custom object
 $obj = [PSCustomObject]@{
     ComputerName = $Name
     OsVersion = $Version
     DiskCount = $DiskCount
+    'GB Free' = $FreeSpace / 1Gb
 }
 
 # show output
