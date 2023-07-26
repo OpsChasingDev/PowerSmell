4a0f524c2a043140826e92be97fdce67aa237632
Robert Stapleton <robert@stapleton-family.net>
2023-07-26 15:03:29 -0400

    starting param definition

--- ServerReport/ServerReport.ps1
+++ ServerReport/ServerReport.ps1
@@ -1,17 +1,22 @@
-# retrieve system info
-$Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
-$Name = $env:COMPUTERNAME
-$DiskInstance = Get-CimInstance CIM_LogicalDisk 
-$DiskCount = $DiskInstance.Count
-$FreeSpace = ($DiskInstance | Where-Object {$_.DeviceID -eq 'C:'}).FreeSpace
-
-# make our own custom object
-$obj = [PSCustomObject]@{
-    ComputerName = $Name
-    OsVersion = $Version
-    DiskCount = $DiskCount
-    'GB Free' = [System.Math]::Round($FreeSpace / 1Gb, 2)
+function Get-PowerSmell {
+    param (
+        $DriveLetter
+    )
+    # retrieve system info
+    $Version = (Get-ComputerInfo | Select-Object OsVersion).OsVersion
+    $Name = $env:COMPUTERNAME
+    $DiskInstance = Get-CimInstance CIM_LogicalDisk 
+    $DiskCount = $DiskInstance.Count
+    $FreeSpace = ($DiskInstance | Where-Object {$_.DeviceID -eq $DriveLetter}).FreeSpace
+    
+    # make our own custom object
+    $obj = [PSCustomObject]@{
+        ComputerName = $Name
+        OsVersion = $Version
+        DiskCount = $DiskCount
+        'GB Free' = [System.Math]::Round($FreeSpace / 1Gb, 2)
+    }
+    
+    # show output
+    Write-Output $obj
 }
-
-# show output
-Write-Output $obj
